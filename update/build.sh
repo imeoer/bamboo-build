908f67d5786360c0ea270589e0c806cb6597a41f
song <imeoer@gmail.com>
2015-01-17 23:27:50 +0800

    fix update build.sh issue

--- update/build.sh
+++ update/build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # clean dir
-rm /root/build/* -R *
+rm /root/build -R *
 mkdir -p /root/build/web
 mkdir -p /root/build/api
 
 # update web
-cd /root/bamboo-web
+cd bamboo-web
 git pull --rebase
+npm install
 gulp release
 mv release/* /root/build/web
 
